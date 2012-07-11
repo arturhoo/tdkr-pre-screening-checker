82bb41dae2b286f76c4e24f451280e4276593306
Artur Rodrigues <arturhoo@gmail.com>
2012-07-11 15:59:50 -0300

    including python script to automate and send mail

---
+++ py_checker.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from subprocess import check_output, call
+from os import remove
+
+try:
+    from email_to_send import *
+except ImportError:
+    sys.exit("No receiver found!")
+
+
+def build_msg(sender_address, receiver_address):
+    msg = open('msg.txt', 'w')
+    msg.write('To: ' + receiver_address)
+    msg.write('\nFrom: ' + sender_address)
+    msg.write('\nSubject: The Dark Knight Pre Screening!!')
+    msg.write('\n\nGo buy it now!')
+
+if __name__ == '__main__':
+    # Execute casperjs
+    output = check_output(['casperjs', 'checker.js'])
+
+    # Get the first line from casper's output, indicates
+    # the number of dates available on the website
+    dates_len = output.split('\n')[0]
+
+    # Get the second line from casper's output
+    next_session_text = output.split('\n')[1]
+    default_next_session_text = open('default_next_session_text.txt',
+                                     'r').read()
+    if dates_len != '8' or default_next_session_text != next_session_text:
+        build_msg()
+        call(['ssmtp', RECEIVER, '<', 'msg.txt'])
+        remove('msg.txt')
